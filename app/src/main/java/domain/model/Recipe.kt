8a3cb7b7d5d86e44501a3619bb2370bec9df6cfa
Leonardo Behnck <leonardo.behnck@gmail.com>
2024-09-01 17:45:01 -0300

    Criação do domain

---
+++ app/src/main/java/domain/model/Recipe.kt
@@ -0,0 +1,8 @@
+package com.example.listadereceitas.domain.model
+
+typealias RecipeDomain = Recipe
+
+data class Recipe (
+    val id : Int = 0,
+    val name : String,
+)
---
+++ app/src/main/java/domain/repository/RecipeRepository.kt
@@ -0,0 +1,8 @@
+package com.example.listadereceitas.domain.repository
+
+import com.example.listadereceitas.domain.model.RecipeDomain
+
+interface RecipeRepository {
+    suspend fun getAll() : List<RecipeDomain>
+    suspend fun insert(recipe: RecipeDomain)
+}
