a438e884f0d1ec5ab1b1fddfdc2a3e36ed492aa3
Leonardo Behnck <leonardo.behnck@gmail.com>
2024-09-02 13:47:20 -0300

    Create DialogScreen.kt

---
+++ app/src/main/java/com/example/listadereceitas/presentation/dialog/DialogScreen.kt
@@ -0,0 +1,52 @@
+package com.example.listadereceitas.presentation.dialog
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun DialogScreen(showDialog: MutableState<Boolean>) {
+  Dialog(onDismissRequest = { showDialog.value = false }) {
+    Card(
+      modifier = Modifier
+        .fillMaxWidth()
+        .height(200.dp)
+        .padding(16.dp),
+      shape = RoundedCornerShape(16.dp),
+    ) {
+      Text(
+        text = "This is a minimal dialog",
+        modifier = Modifier
+          .fillMaxSize()
+          .wrapContentSize(Alignment.Center),
+        textAlign = TextAlign.Center,
+      )
+    }
+  }
+}
+
+
+@Preview
+@Composable
+fun Preview() {
+  //DialogScreen()
+}
+
+
+
