b7bbe921592d721dbe6d575b69858b5845fffd2c
Leonardo Behnck <leonardo.behnck@gmail.com>
2024-09-02 10:06:09 -0300

    ajuste no dao

--- app/src/main/java/com/example/listadereceitas/data/dao/RecipeDao.kt
+++ app/src/main/java/com/example/listadereceitas/data/dao/RecipeDao.kt
@@ -1,6 +1,9 @@
-package com.example.listadereceitas.data
+package com.example.listadereceitas.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.listadereceitas.data.entity.RecipeEntity
 
 @Dao
 interface RecipeDao {
--- app/src/main/java/com/example/listadereceitas/data/entity/Recipe.kt
+++ app/src/main/java/com/example/listadereceitas/data/entity/Recipe.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-typealias RecipeEntity
+typealias RecipeEntity = Recipe
 
 @Entity
 data class Recipe(
