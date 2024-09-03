6196249d3a628268bf3b48a31923cc915cc34f4e
Leonardo Behnck <leonardo.behnck@gmail.com>
2024-09-03 11:24:52 -0300

    implementação do dialog

--- app/src/main/java/com/example/listadereceitas/presentation/dialog/DialogScreen.kt
+++ app/src/main/java/com/example/listadereceitas/presentation/dialog/DialogScreen.kt
@@ -1,45 +1,56 @@
 package com.example.listadereceitas.presentation.dialog
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun DialogScreen(showDialog: MutableState<Boolean>) {
-  Dialog(onDismissRequest = { showDialog.value = false }) {
-    Card(
-      modifier = Modifier
-        .fillMaxWidth()
-        .height(200.dp)
-        .padding(16.dp),
-      shape = RoundedCornerShape(16.dp),
-    ) {
-      Text(
-        text = "This is a minimal dialog",
-        modifier = Modifier
-          .fillMaxSize()
-          .wrapContentSize(Alignment.Center),
-        textAlign = TextAlign.Center,
+  var title by remember { mutableStateOf("")  }
+  AlertDialog(
+    title = {
+      Text(text = "Adicione uma receita")
+    },
+    text = {
+      OutlinedTextField(
+        value = title,
+        onValueChange = { title = it },
+        label = { Text("Insira um titulo:") }
       )
+    },
+    onDismissRequest = {
+      showDialog.value = false
+                       },
+    confirmButton = {
+      TextButton(
+        onClick = {
+          showDialog.value = false
+        }
+      ) {
+        Text("Confirm")
+      }
+    },
+    dismissButton = {
+      TextButton(
+        onClick = {
+          showDialog.value = false
+        }
+      ) {
+        Text("Dismiss")
+      }
     }
-  }
-
+  )
 }
 
 
